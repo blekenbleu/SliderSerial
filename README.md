da255cb0431fee3b0bd4947b6cba42b7f82ed0f4
Steven Carmichael <blekenbleu@gmail.com>
2019-02-11 17:03:09 -0500

    initial clone of C#Corner WPF Slider Control by Mahesh Chand

---
+++ README.md
@@ -0,0 +1,10 @@
+11 February 2019
+## SliderSerial
+Build a test app that translates slider settings to IBUS data  
+for testing VJoySerialFeeder without a real device, via virtual serial ports.
+
+Slider source is from Mahesh Chand's [WPF Slider Control](https://www.c-sharpcorner.com/uploadfile/mahesh/slider-control-in-wpf/)  
+Application source described there is `SliderSample/MainWindow.xaml`  
+
+
+See [GitHub Pages website](https://blekenbleu.github.io/) for published documentation.  
---
+++ SliderSample/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SliderSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RedSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateCircleWithColors();
+        }
+
+        private void GreenSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateCircleWithColors();
+        }
+
+        private void BlueSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateCircleWithColors();
+        }
+
+        private void UpdateCircleWithColors()
+        {
+            Color clr = Color.FromArgb(255, Convert.ToByte(RedSlider.Value),
+                Convert.ToByte(GreenSlider.Value), Convert.ToByte(BlueSlider.Value));
+            mcCircle.Fill = new SolidColorBrush(clr);
+        }
+
+    }
+}
